03b784566d645c53694b509eb178e376d0ff38e4
mkhong210 <ghdalsrud210@gmail.com>
2023-08-30 09:45:36 +0900

    08.30 morning update

--- js/enjoy.js
+++ js/enjoy.js
@@ -1,3 +1,121 @@
+// attraction.html
+const fetchAttract = function() {
+	fetch("../../data/enjoy/attraction.json")
+	.then(res => res.json())
+	.then(data => {
+		const tabMenu = document.querySelector(".tab_menu");
+		const attractContent = document.querySelector(".attraction_list")
+		let tabBtn;
+		
+		for (const dataKey in data) {
+			tabMenu.innerHTML += `<a class="tabkey">${dataKey}</a>`;
+		}
+		tabBtn = document.querySelectorAll(".tabkey");
+
+		// 미리 뿌려놓기 
+		let num = 1;
+		data.아메리칸어드벤처.forEach(function (V, K) {
+			tabBtn[1].classList.add("active");
+			attractContent.innerHTML += `
+				<li onClick="modalOn(${V.id}, '아메리칸어드벤처')">
+					<div class="img_area"></div>
+					<div class="txt_area">
+						<b>${V.name}</b>
+						<p>#${V.location}</p>
+					</div>
+				</li>`;
+			
+			const thumbImg = document.querySelectorAll('.img_area');
+			thumbImg[K].style = ` background-image:url(${V.thumb})`;
+		})
+
+		// tab 눌렀을 시
+		tabBtn.forEach(function (V, K) {
+			V.addEventListener("click", function () {
+				// e.stopPropagation();
+				tabBtn[num].classList.remove('active');
+				this.classList.add('active');
+				num = K;
+	
+				attractContent.innerHTML = "";
+				const buttonText = V.innerText;
+				data[buttonText].forEach(function (dataV, dataK) {
+					// console.log(this)
+					attractContent.innerHTML += `
+					<li onClick="modalOn(${dataV.id}, '${buttonText}')">
+						<div class="img_area"></div>
+						<div class="txt_area">
+							<b>${dataV.name}</b>
+							<p>#${dataV.location}</p>
+						</div>
+					</li>`;
+					
+					const viewImg = document.querySelectorAll('.img_area');
+					viewImg[dataK].style = ` background-image:url(${dataV.thumb})`;
+				})
+			})
+		})
+		
+		const modal = document.querySelector(".modal_wrap");
+		const body = document.querySelector("body");
+		modal.classList.remove("open");
+		body.classList.remove("hide");
+	})
+}
+
+// attraction_modal
+const modalOn = function(vID, activeTab) {	
+	const attraction = document.querySelector(".modal_sec");
+	const modal = document.querySelector(".modal_wrap");
+	const body = document.querySelector("body");
+	const closeBtn = document.querySelector(".close");
+
+	modal.classList.add("open");
+	body.classList.add("hide");
+
+	closeBtn.addEventListener("click", function(e) {
+		modal.classList.remove("open");
+		body.classList.remove("hide");
+	})
+
+	fetch("../../data/enjoy/attraction.json")
+	.then(res => res.json())
+	.then(data => {
+		console.log(activeTab);
+
+		
+		const resData = data[activeTab].find((e) => e.id == vID);
+		if (resData) {
+				console.log(resData.name);
+				// 모달 창 내용 채우기
+				document.querySelector('.modalLoca').innerText = '#'+resData.location;
+				document.querySelector('.modalTitle').innerText = resData.name;
+				document.querySelector('.pic img').src = resData.detail_img;
+				document.querySelector('.modalDesc').innerText = resData.desc;
+				console.log(resData.use_height)
+				if(resData.use_height) {
+					document.querySelector('.modalKey').style.display = 'block'
+					document.querySelector('.modalKey').innerText = '이용 가능 키 제한 : '+ resData.use_height;
+				} else {
+					document.querySelector('.modalKey').style.display = 'none'
+				}
+				if(resData.use_info) {
+					document.querySelector('.modalInfo').style.display = 'block'
+					document.querySelector('.modalInfo').innerText = '[이용안내]\n'+ resData.use_info;
+				} else {
+					document.querySelector('.modalInfo').style.display = 'none'
+				}
+				if(resData.attention) {
+					document.querySelector('.modalAtten').style.display = 'block'
+					document.querySelector('.modalAtten').innerText = '[유의사항]\n'+ resData.attention;
+				} else {
+					document.querySelector('.modalAtten').style.display = 'none'
+				}
+				// 다른 필요한 데이터도 위와 같이 추가해주세요
+		}
+	})
+}
+
 // view.html
 const fetchView = function() {
 	fetch("../../data/enjoy/view.json")
@@ -16,7 +134,7 @@ const fetchView = function() {
 		data.주토피아.forEach(function(v, k) {
 			tabBtn[0].classList.add("active");
 			viewBox.innerHTML += `
-				<li>
+				<li onClick="modalOn_view(${v.id}, '주토피아')">
 					<div class="img_area"></div>
 					<div class="txt_area">
 						<b>${v.name}</b>
@@ -36,11 +154,10 @@ const fetchView = function() {
 
 				viewBox.innerHTML = '';
 				const btnText = val.innerText;
-				console.log(btnText)
 
 				data[btnText].forEach((dataV, dataK)  => {
 					viewBox.innerHTML += 
-					`<li>
+					`<li onClick="modalOn_view(${dataV.id}, '${btnText}')">
 						<div class="img_area"></div>
 						<div class="txt_area">
 							<b>${dataV.name}</b>
@@ -54,65 +171,64 @@ const fetchView = function() {
 				})
 			})
 		})
+		const modal = document.querySelector(".modal_wrap");
+		const body = document.querySelector("body");
+		modal.classList.remove("open");
+		body.classList.remove("hide");
 	})
 } // view
 
-// attraction.html
-const fetchAttract = function() {
-	fetch("../../data/enjoy/attraction.json")
+// view_modal
+const modalOn_view = function(vID, activeTab) {	
+	const attraction = document.querySelector(".modal_sec");
+	const modal = document.querySelector(".modal_wrap");
+	const body = document.querySelector("body");
+	const closeBtn = document.querySelector(".close");
+
+	modal.classList.add("open");
+	body.classList.add("hide");
+
+	closeBtn.addEventListener("click", function(e) {
+		modal.classList.remove("open");
+		body.classList.remove("hide");
+	})
+
+	fetch("../../data/enjoy/view.json")
 	.then(res => res.json())
 	.then(data => {
-		const tabMenu = document.querySelector(".tab_menu");
-		const attractContent = document.querySelector(".attraction_list")
-		let tabBtn;
-		
-		for (const dataKey in data) {
-			tabMenu.innerHTML += `<a class="tabkey">${dataKey}</a>`;
+		console.log(activeTab);
+
+		const resData = data[activeTab].find((e) => e.id == vID);
+		if (resData) {
+				console.log(resData.name);
+				// 모달 창 내용 채우기
+				document.querySelector('.modalLoca').innerText = '#'+resData.location;
+				document.querySelector('.modalTitle').innerText = resData.name;
+				document.querySelector('.pic img').src = resData.thumb;
+				document.querySelector('.modalDesc').innerText = resData.desc;
+				if(resData.event) {
+					document.querySelector('.modalEvent').style.display = 'block'
+					document.querySelector('.modalEvent').innerText = '[이벤트]\n'+ resData.event;
+				} else {
+					document.querySelector('.modalEvent').style.display = 'none'
+				}
+				if(resData.attention) {
+					document.querySelector('.modalInfo').style.display = 'block'
+					document.querySelector('.modalInfo').innerText = '[이용안내]\n'+ resData.attention;
+				} else {
+					document.querySelector('.modalInfo').style.display = 'none'
+				}
+				if(resData.warning) {
+					document.querySelector('.modalAtten').style.display = 'block'
+					document.querySelector('.modalAtten').innerText = '[유의사항]\n'+ resData.warning;
+				} else {
+					document.querySelector('.modalAtten').style.display = 'none'
+				}
+				// 다른 필요한 데이터도 위와 같이 추가해주세요
 		}
-		tabBtn = document.querySelectorAll(".tabkey");
-
-		let num = 1;
-		data.아메리칸어드벤처.forEach(function (V, K) {
-			tabBtn[1].classList.add("active");
-			attractContent.innerHTML += `
-				<li>
-					<div class="img_area"></div>
-					<div class="txt_area">
-						<b>${V.name}</b>
-						<p>#${V.location}</p>
-					</div>
-				</li>`;
-			
-			const thumbImg = document.querySelectorAll('.img_area');
-			thumbImg[K].style = ` background-image:url(${V.thumb})`;
-		})
-
-		tabBtn.forEach(function (V, K) {
-			V.addEventListener("click", function () {
-				tabBtn[num].classList.remove('active');
-				this.classList.add('active');
-				num = K;
-	
-				attractContent.innerHTML = "";
-				const buttonText = V.innerText;
-				data[buttonText].forEach(function (dataV, dataK) {
-					console.log(this)
-					attractContent.innerHTML += `
-					<li>
-						<div class="img_area"></div>
-						<div class="txt_area">
-							<b>${dataV.name}</b>
-							<p>#${dataV.location}</p>
-						</div>
-					</li>`;
-					
-					const viewImg = document.querySelectorAll('.img_area');
-					viewImg[dataK].style = ` background-image:url(${dataV.thumb})`;
-				})
-			})
-		})
 	})
 }
+
 
 // rest_presnt.html
 const fetchRest = function() {
--- js/index.js
+++ js/index.js
@@ -3,17 +3,14 @@ new fullpage('#fullpage', {
 		console.log(destination.index)
         if (destination.index == 0) {
                 $("header").addClass("active");
-            } else {
-                $("header").removeClass("active");
-            }
-
-		if (destination.index != 0) {
-                $(".gnb > li > a").removeClass("active");
-                $(".gnb_right").removeClass("active");
-            } else {
                 $(".gnb > li > a").addClass("active");
                 $(".gnb_right").addClass("active");
+            } else {
+                $("header").removeClass("active");
+                $(".gnb > li > a").removeClass("active");
+                $(".gnb_right").removeClass("active");
             }
+
 
         $(".section.active").find(".inner .main_title").addClass("active");
         $(".section.active").find(".inner .promotion-contents").addClass("active");
@@ -45,13 +42,13 @@ var swiper = new Swiper(".sec1Swiper", {
         },
         slideChange: function(e) {
             var currentSlideIndex = e.activeIndex;
-            if (currentSlideIndex == 3 || currentSlideIndex == 4) {
-                $(".gnb > li > a").addClass("active");
-                $(".gnb_right").addClass("active");
-            } else {
-                $(".gnb > li > a").removeClass("active");
-                $(".gnb_right").removeClass("active");
-            }			
+            // if (currentSlideIndex == 3 || currentSlideIndex == 4) {
+            //     $(".gnb > li > a").addClass("active");
+            //     $(".gnb_right").addClass("active");
+            // } else {
+            //     $(".gnb > li > a").removeClass("active");
+            //     $(".gnb_right").removeClass("active");
+            // }			
         }
     }
 });
@@ -62,6 +59,9 @@ var swiper = new Swiper(".sec2Swiper", {
     spaceBetween: 20,
     loop: true,
     speed: 800,
+    autoplay: {
+        delay: 5000, // 5초마다 슬라이드 전환
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -78,6 +78,9 @@ var swiper = new Swiper(".sec4Swiper", {
     spaceBetween: 30,
     loop: true,
     speed: 800,
+    autoplay: {
+        delay: 5000, // 5초마다 슬라이드 전환
+    },
     loopAdditionalSlides : 4,
     navigation: {
         nextEl: ".swiper-button-next",
@@ -109,7 +112,6 @@ const wText1 = document.querySelector(".wText1");
 const wText2 = document.querySelector(".wText2");
 const wText3 = document.querySelector(".wText3");
 const wText4 = document.querySelector(".wText4");
-const wText5 = document.querySelector(".wText5");
 const dateTag = document.querySelector(".section5 .info div p:nth-of-type(1)")
 
 
@@ -139,7 +141,7 @@ fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst
     //         default : wText2.innerText = "일시적으로 불러올 수 없습니다.";
     //     }
     // }
-    // setInterval(submitSky, 500);
+    // setInterval(submitSky, 3000);
 })
 
 
@@ -175,7 +177,7 @@ fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst
             default : wText3.innerText = `날씨 불러올 수 없음`;
         }
     }
-    setInterval(submitTemp, 500);
+    setInterval(submitTemp, 3000);
 }
 )
 
@@ -187,12 +189,11 @@ fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?s
     const tmx = data.response.body.items.item[120].fcstValue;
 
     function submitRainy() {
-        wText2.innerText = `강수확률${rainy}%`
-        wText4.innerText = `최저${tmn}°C`
-        wText5.innerText = `최고${tmx}°C`
+        wText2.innerText = `강수확률 ${rainy}%`
+        wText4.innerText = `최저${tmn}°C / 최고${tmx}°C`
         
     }
-    setInterval(submitRainy, 500);
+    setInterval(submitRainy, 3000);
 })
 
 
