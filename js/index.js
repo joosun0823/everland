b95053bec8cdc1373f7d6e5d96c2ba07a1726404
mkhong210 <ghdalsrud210@gmail.com>
2023-08-30 10:37:08 +0900

    08.30 main update

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ new fullpage('#fullpage', {
                 $(".gnb_right").removeClass("active");
             }
 
-
         $(".section.active").find(".inner .main_title").addClass("active");
         $(".section.active").find(".inner .promotion-contents").addClass("active");
         $(".section.active").find("h2").addClass("active");
@@ -59,6 +58,21 @@ var swiper = new Swiper(".sec2Swiper", {
     spaceBetween: 20,
     loop: true,
     speed: 800,
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        }
+    },
     autoplay: {
         delay: 5000, // 5초마다 슬라이드 전환
     },
@@ -68,7 +82,7 @@ var swiper = new Swiper(".sec2Swiper", {
     },
     scrollbar: {
     el: ".swiper-scrollbar",
-    hide: false,
+    hide: true,
     },
 });
 
@@ -78,6 +92,25 @@ var swiper = new Swiper(".sec4Swiper", {
     spaceBetween: 30,
     loop: true,
     speed: 800,
+    breakpoints: {
+        // 화면 폭이 768px 이상일 때
+        0: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        380: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        768: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        1200: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        }
+    },
     autoplay: {
         delay: 5000, // 5초마다 슬라이드 전환
     },
@@ -157,22 +190,26 @@ fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst
         switch(weather) {
             case "0" : 
             wText3.innerText = `현재 하늘 맑음`;
-            weatherIcon.src = "../asset/image/main/weather_sun.png";
+            weatherIcon.src = "./asset/image/main/weather_sun.png";
             break;
+
             case "1" : 
             wText3.innerText = `현재 비 옴`;
-            weatherIcon.src = "../asset/image/main/weather_rainy.png";
+            weatherIcon.src = "./asset/image/main/weather_rainy.png";
             break;
+
             // case "2" : wText5.innerText = `현재 비와 눈 옴`; break;
             // case "3" : wText5.innerText = `현재 눈 옴`; break;
             case "5" : 
             wText3.innerText = "현재 하늘 흐림⛅";
-            weatherIcon.src = "../asset/image/main/weather_tinyCloud.png";
+            weatherIcon.src = "./asset/image/main/weather_tinyCloud.png";
             break;
+
             case "6" :
             wText3.innerText = `현재 바람 주의`;
-            weatherIcon.src = "../asset/image/main/weather_windy.png";
+            weatherIcon.src = "./asset/image/main/weather_windy.png";
             break;
+
             // case "7" : wText5.innerText = `현재 눈`; break;
             default : wText3.innerText = `날씨 불러올 수 없음`;
         }
@@ -189,7 +226,7 @@ fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?s
     const tmx = data.response.body.items.item[120].fcstValue;
 
     function submitRainy() {
-        wText2.innerText = `강수확률 ${rainy}%`
+        wText2.innerText = `오늘의 강수확률 ${rainy}%`
         wText4.innerText = `최저${tmn}°C / 최고${tmx}°C`
         
     }
